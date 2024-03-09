c1e8d2ce50cf94c2baaa131ebab2b58d15bf5f22
Mohammad Shahnawaz <shahnawazmd791@gmail.com>
2024-03-09 19:33:27 +0530

    upgrade few more packages

--- src/db/Connection.js
+++ src/db/Connection.js
@@ -7,7 +7,7 @@ const Connection = async () => {
   try {
     await mongoose.connect(
       `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@ecommerce-app.x82kj.mongodb.net/ecommerce-detail?retryWrites=true&w=majority`,
-      { useNewUrlParser: true, useUnifiedTopology: true }
+      { autoIndex: true }
     );
     console.log('Database Connected');
   } catch (err) {
